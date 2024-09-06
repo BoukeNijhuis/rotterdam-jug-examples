26b185bbf90f47a600a4b4a20548bfce28aa31a4
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2024-09-06 19:42:07 +0200

    dummy implementation

--- src/main/java/org/example/primenumber/PrimeNumberGenerator.java
+++ src/main/java/org/example/primenumber/PrimeNumberGenerator.java
@@ -1,29 +1,10 @@
 package org.example.primenumber;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeNumberGenerator {
 
     public List<Integer> generate(int max) {
-        List<Integer> primes = new ArrayList<>();
-        for (int num = 2; num <= max; num++) {
-            if (isPrime(num)) {
-                primes.add(num);
-            }
-        }
-        return primes;
-    }
-
-    private boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2, sqrt = (int) Math.sqrt(num); i <= sqrt; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        return null;
     }
 }
